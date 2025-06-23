c89bf7c5e36bf0c8f3284644e507076b4907e79a
dikad911 <dwi.andika@kbru.co.id>
2025-06-23 17:05:58 +0700

    Adjust UI portfolio

--- src/components/Experience/index.vue
+++ src/components/Experience/index.vue
@@ -27,12 +27,12 @@
               </p>
               <div v-if="item.project">
                 <div v-for="(project, j) in item.project" :key="j" class="project-link">
-                  <a :href="project.url" target="_blank">
+                  <a :href="project.url" target="_blank" style="text-decoration: none;">
                     <div class="project-item" @click="handleProjectLink(i, j)">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                         <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/>
                       </svg>
-                      <p>{{ project.name }}</p>
+                      <p style="margin-right: 20px">{{ project.name }}</p>
                     </div>
                   </a>
                 </div>
@@ -59,15 +59,19 @@ export default {
         {
           date: '2022 — Present',
           note: '',
-          role: 'Frontend Engineer, PT.Kali Besar Raya Utama · Jagain.com',
-          description: 'Develop and maintain key components for both the front office and back office (dashboard) of the Jagain.com insurance website. Work closely with cross-functional teams, including developers, designers, and product managers, to create seamless, user-friendly experiences for both customers and internal users. Advocate for best practices in web accessibility, performance, and security across the entire platform.',
-          tech: ['Javascript', 'Nuxt', 'Vue', 'React', 'HTML & SCSS'],
+          role: 'Frontend Engineer, PT.Kali Besar Raya Utama',
+          description: 'Develop and maintain key components for both the front office and back office (dashboard) of the Jagain.com insurance website. Work closely with cross-functional teams, including developers, designers, and product managers, to create seamless, user-friendly experiences for both customers and internal users. Advocate for best practices in web accessibility, performance, and security across the entire platform.Also contributed to SmartPremi, a PHP-based B2B insurance platform, by developing dynamic insurance forms tailored to business clients. Ensured scalability, maintainability, and optimal user experience in form creation and data handling processes.',
+          tech: ['Javascript', 'PHP',  'Nuxt', 'Vue', 'React', 'HTML & SCSS'],
           url: 'https://www.kbru.co.id/',
           project: [
             {
               name: 'jagain.com',
               url: 'https://jagain.com'
-            }
+            },
+            {
+              name: 'smartpremi.com',
+              url: 'https://smartpremi.com'
+            },
           ]
         },
         {
--- src/components/Projects/index.vue
+++ src/components/Projects/index.vue
@@ -4,46 +4,44 @@
       <div class="title-mobile">
         <h4 class="title">Projects</h4>
       </div>
-      <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusamus commodi atque? Illo, similique. Voluptatem earum a quaerat alias 
-        labore quas reprehenderit dolorem iure ut at voluptate, expedita mollitia ullam pariatur, molestiae illum necessitatibus rem! Assumenda, dolore. 
-        Repellat impedit veniam nesciunt doloribus qui expedita. Ducimus dolorem hic sapiente non inventore corrupti necessitatibus unde, beatae ratione nisi. 
-        Debitis, sint exercitationem. Itaque illo dicta ad dolorum praesentium! Delectus commodi ullam aut quis omnis necessitatibus doloribus quas, 
-        recusandae natus tempora enim et voluptates obcaecati adipisci, optio ea cum, ratione sit non deleniti blanditiis voluptatibus expedita cumque tempore. 
-        Obcaecati velit deleniti quos ipsa eveniet aut magni ut expedita asperiores amet. Placeat, cupiditate voluptate obcaecati beatae sed odio ipsa sequi laboriosam 
-        laudantium temporibus quas repellendus consequatur perspiciatis odit delectus eveniet ea laborum commodi iusto maxime dicta dolorum! Exercitationem, quos nobis. 
-        Dolores praesentium necessitatibus quo debitis illo officiis modi perferendis quibusdam, placeat minus laboriosam sed aliquid vero quidem voluptates tempora, ab dolorum. 
-        Officiis tenetur aliquam architecto quaerat deleniti error soluta, temporibus magnam nihil tempore? Non iusto sequi ab dolores velit quod cumque officiis temporibus quisquam modi vel dicta fugiat, 
-        quos ea tenetur architecto ratione culpa quidem error reprehenderit consequatur! Id cupiditate perspiciatis dolores hic dignissimos nemo.
-      </p>
-      <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusamus commodi atque? Illo, similique. Voluptatem earum a quaerat alias 
-        labore quas reprehenderit dolorem iure ut at voluptate, expedita mollitia ullam pariatur, molestiae illum necessitatibus rem! Assumenda, dolore. 
-        Repellat impedit veniam nesciunt doloribus qui expedita. Ducimus dolorem hic sapiente non inventore corrupti necessitatibus unde, beatae ratione nisi. 
-        Debitis, sint exercitationem. Itaque illo dicta ad dolorum praesentium! Delectus commodi ullam aut quis omnis necessitatibus doloribus quas, 
-        recusandae natus tempora enim et voluptates obcaecati adipisci, optio ea cum, ratione sit non deleniti blanditiis voluptatibus expedita cumque tempore. 
-        Obcaecati velit deleniti quos ipsa eveniet aut magni ut expedita asperiores amet. Placeat, cupiditate voluptate obcaecati beatae sed odio ipsa sequi laboriosam 
-        laudantium temporibus quas repellendus consequatur perspiciatis odit delectus eveniet ea laborum commodi iusto maxime dicta dolorum! Exercitationem, quos nobis. 
-        Dolores praesentium necessitatibus quo debitis illo officiis modi perferendis quibusdam, placeat minus laboriosam sed aliquid vero quidem voluptates tempora, ab dolorum. 
-        Officiis tenetur aliquam architecto quaerat deleniti error soluta, temporibus magnam nihil tempore? Non iusto sequi ab dolores velit quod cumque officiis temporibus quisquam modi vel dicta fugiat, 
-        quos ea tenetur architecto ratione culpa quidem error reprehenderit consequatur! Id cupiditate perspiciatis dolores hic dignissimos nemo.
-      </p>
-      <p class="about-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem accusamus commodi atque? Illo, similique. Voluptatem earum a quaerat alias 
-        labore quas reprehenderit dolorem iure ut at voluptate, expedita mollitia ullam pariatur, molestiae illum necessitatibus rem! Assumenda, dolore. 
-        Repellat impedit veniam nesciunt doloribus qui expedita. Ducimus dolorem hic sapiente non inventore corrupti necessitatibus unde, beatae ratione nisi. 
-        Debitis, sint exercitationem. Itaque illo dicta ad dolorum praesentium! Delectus commodi ullam aut quis omnis necessitatibus doloribus quas, 
-        recusandae natus tempora enim et voluptates obcaecati adipisci, optio ea cum, ratione sit non deleniti blanditiis voluptatibus expedita cumque tempore. 
-        Obcaecati velit deleniti quos ipsa eveniet aut magni ut expedita asperiores amet. Placeat, cupiditate voluptate obcaecati beatae sed odio ipsa sequi laboriosam 
-        laudantium temporibus quas repellendus consequatur perspiciatis odit delectus eveniet ea laborum commodi iusto maxime dicta dolorum! Exercitationem, quos nobis. 
-        Dolores praesentium necessitatibus quo debitis illo officiis modi perferendis quibusdam, placeat minus laboriosam sed aliquid vero quidem voluptates tempora, ab dolorum. 
-        Officiis tenetur aliquam architecto quaerat deleniti error soluta, temporibus magnam nihil tempore? Non iusto sequi ab dolores velit quod cumque officiis temporibus quisquam modi vel dicta fugiat, 
-        quos ea tenetur architecto ratione culpa quidem error reprehenderit consequatur! Id cupiditate perspiciatis dolores hic dignissimos nemo.
-      </p>
+      <div class="list-container">
+        <div v-for="(item, i) in projectsData" :key="i">
+          <a :href="item.url" target="_blank" class="projects-card">
+            <div class="projects-description">
+              <div class="projects-header">
+                <span class="projects-title">
+                  {{ item.name }}
+                </span>
+              </div>
+            </div>
+            <div class="projects-tech" v-if="item.tech">
+              <div class="tech-list" v-for="list in item.tech" :key="list">
+                {{ list }}
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
+buat
 <script>
 export default {
-  name: 'AboutComponent'
+  name: 'AboutComponent',
+  data() {
+    return {
+      projectsData: [
+        {
+          name: 'Foxinest',
+          description: '',
+          url: 'https://foxi-todo.netlify.app/',
+          img: '',
+          tech: ['React', 'Material UI', 'Netlify']
+        }
+      ]
+    }
+  }
 }
 </script>
 
